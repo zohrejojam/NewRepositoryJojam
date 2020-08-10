a922dab0cefab95edcb2ba9e994004ef0f82348c
zohrejojam <zohrejojam297@gmail.com>
2020-08-10 17:38:13 +0430

    Add to new Repository

--- OnlineStoreCore/Services/AccountingDocumentService.cs
+++ OnlineStoreCore/Services/AccountingDocumentService.cs
@@ -18,7 +18,6 @@ namespace OnlineStoreCore.Services
 
         public string CreateDocumentNumber()
         {
-            int test = 0;
             return DateTime.Now.ToString("yyyyMMdd") +
                    DateTime.Now.Hour.ToString() +
                    DateTime.Now.Minute.ToString() +
